d8b5ea669a91a7e2755b3d4af60bbcfda6ac4f29
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-14 11:00:12 +0300

    response

--- js/main.js
+++ js/main.js
@@ -10,6 +10,19 @@ function getMovies(searchText) {
     axios.get('https://thesimpsonsquoteapi.glitch.me/quotes?count='+searchText)
         .then((response) => {
             console.log(response)
+            let quotes = response.data;
+            let output = '';
+            $.each(quotes, (index, quote) => {
+                output += `
+                    <div class="col-md-3>
+                        <div class="well text-center">
+                            <h5>${quote.quote}</h5>
+                            <img src=${quote.image}>
+                        </div>
+                    </div>
+                `;
+            })
+            $('#quotes').html(output)
         })
         .catch((err) => {
             console.log(err)
