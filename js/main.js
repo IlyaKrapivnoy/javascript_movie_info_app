896828a807d844dcb6b7593e225b70a25bf29f09
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-14 14:38:44 +0300

    small fixes

--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ function getMovies(searchText) {
                 output += 
                     `<div class="col-4 p-4">
                         <div class="well text-left">
-                            <img src=${quote.image}>
+                            <div class="text-center">
+                                <img src=${quote.image}>
+                            </div>
                             <p class="resQuote pt-4">${quote.quote}</p>
                             <cite class="text-right">${quote.character}</cite>
                         </div>
@@ -30,42 +32,4 @@ function getMovies(searchText) {
         })
 }
 
-            // $.each(quotes, (index, quote) => {
-            //     output += 
-            //         `<div class="col-4 p-4">
-            //             <div class="well text-left">
-            //                 <img src=${quote.image}>
-            //                 <p class="resQuote pt-4">${quote.quote}</p>
-            //                 <cite class="text-right">${quote.character}</cite>
-            //             </div>
-            //         </div>`
-            // })
- 
-
-
-
-    
-// let output = '';
-// $.each(quotes, (index, quote) => {
-//     output += 
-//         `<div class="col-4 p-4">
-//             <div class="well text-left">
-//                 <img src=${quote.image}>
-//                 <p class="resQuote pt-4">${quote.quote}</p>
-//                 <cite class="text-right">${quote.character}</cite>
-//             </div>
-//         </div>`
-
-    //  if(output == '') {
-    //     output = `<img src='img/homer.png' />`
-    //  } else {
-    //     output = `<div class="col-4 p-4">
-    //     <div class="well text-left">
-    //         <img src=${quote.image}>
-    //         <p class="resQuote pt-4">${quote.quote}</p>
-    //         <cite class="text-right">${quote.character}</cite>
-    //     </div>
-    // </div>`
-    //  }
-     
- 
+         
