d036410c2960e212a03eec2c970e85736abddf04
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-14 14:05:50 +0300

    loader

--- js/main.js
+++ js/main.js
@@ -13,15 +13,15 @@ function getMovies(searchText) {
             let quotes = response.data;
             let output = '';
             $.each(quotes, (index, quote) => {
-                output += `
-                    <div class="col-4 p-4">
+                output += 
+                    `<div class="col-4 p-4">
                         <div class="well text-left">
                             <img src=${quote.image}>
                             <p class="resQuote pt-4">${quote.quote}</p>
                             <cite class="text-right">${quote.character}</cite>
                         </div>
-                    </div>
-                `;
+                    </div>`
+                    $('.loader').hide()
             })
             $('#quotes').html(output)
         })
@@ -29,6 +29,43 @@ function getMovies(searchText) {
             console.log(err)
         })
 }
+
+            // $.each(quotes, (index, quote) => {
+            //     output += 
+            //         `<div class="col-4 p-4">
+            //             <div class="well text-left">
+            //                 <img src=${quote.image}>
+            //                 <p class="resQuote pt-4">${quote.quote}</p>
+            //                 <cite class="text-right">${quote.character}</cite>
+            //             </div>
+            //         </div>`
+            // })
  
 
+
+
+    
+// let output = '';
+// $.each(quotes, (index, quote) => {
+//     output += 
+//         `<div class="col-4 p-4">
+//             <div class="well text-left">
+//                 <img src=${quote.image}>
+//                 <p class="resQuote pt-4">${quote.quote}</p>
+//                 <cite class="text-right">${quote.character}</cite>
+//             </div>
+//         </div>`
+
+    //  if(output == '') {
+    //     output = `<img src='img/homer.png' />`
+    //  } else {
+    //     output = `<div class="col-4 p-4">
+    //     <div class="well text-left">
+    //         <img src=${quote.image}>
+    //         <p class="resQuote pt-4">${quote.quote}</p>
+    //         <cite class="text-right">${quote.character}</cite>
+    //     </div>
+    // </div>`
+    //  }
+     
  
